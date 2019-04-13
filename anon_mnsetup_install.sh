d246ae134e2852aeadf3843f4d19e75780ed33e9
dk808hth <44477250+dk808hth@users.noreply.github.com>
2019-04-12 18:40:49 -0700

    Update anon_mnsetup_install.sh

--- anon_mnsetup_install.sh
+++ anon_mnsetup_install.sh
@@ -3,64 +3,73 @@
 COIN_NAME='ANON' #no spaces
 
 #wallet information
-WALLET_DOWNLOAD='https://www.dropbox.com/s/raw/i0zslq200u6w797/anon-linux.zip'
-WALLET_TAR_FILE='anon-linux.zip'
-ZIPTAR='unzip' #can be either unzip or tar -xfzg
+WALLET_DOWNLOAD='https://github.com/anonymousbitcoin/anon/releases/download/v2.2.0/Anon-full-node-v.2.2.0-ubuntu-18.tar.gz'
 EXTRACT_DIR='' #not always necessary, can be blank if zip/tar file has no subdirectories
 CONFIG_FOLDER='/root/.anon'
 CONFIG_FILE='anon.conf'
 COIN_DAEMON='anond'
 COIN_CLI='anon-cli'
 COIN_PATH='/usr/bin'
-ADDNODE1='172.245.97.67'
-ADDNODE2='204.152.210.202'
-ADDNODE3='96.126.112.77'
 PORT='33130'
-RPCPORT='19050'
+RPCPORT='33129'
+SSHPORT=22
+USERNAME=$LOGNAME
 
-BOOTSTRAP='https://www.dropbox.com/s/raw/qpiptewnx6i7jhc/anon-bootstrap.zip'
+WANIP=$(wget http://ipecho.net/plain -O - -q)
+
+BOOTSTRAP='http://assets.anonfork.io/anon-bootstrap.zip'
 BOOTSTRAP_ZIP='anon-bootstrap.zip'
 
 FETCHPARAMS='https://raw.githubusercontent.com/anonymousbitcoin/anon/master/anonutil/fetch-params.sh'
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
 
 #end of required details
 #
 #
 #
 
-echo "=================================================================="
-echo "$COIN_NAME MN DEFAULT INSTALLER"
-echo "=================================================================="
-echo "Installing packages and updates..."
-sudo apt-get update -y
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:bitcoin/bitcoin -y
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
-sudo apt-get install git -y
-sudo apt-get install curl -y
-sudo apt-get install nano -y
-sudo apt-get install pwgen -y
-sudo apt-get install ufw -y
-sudo apt-get install dnsutils -y
-sudo apt-get install build-essential libtool autotools-dev pkg-config libssl-dev -y
-sudo apt-get install  libc6-dev m4 g++-multilib -y
-sudo apt-get install autoconf libtool ncurses-dev unzip git python -y
-sudo apt-get install zlib1g-dev wget bsdmainutils automake -y
-sudo apt-get install libboost-all-dev -y
-sudo apt-get install libevent-dev -y
-sudo apt-get install libminiupnpc-dev -y
-sudo apt-get install libzmq3-dev -y
-sudo apt-get install autoconf -y
-sudo apt-get install automake -y
-sudo apt-get install unzip -y
-sudo apt-get update
-sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
-sudo apt-get install libminiupnpc-dev libzmq3-dev libevent-pthreads-2.0-5 -y
-sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev -y
-sudo apt-get install libqrencode-dev bsdmainutils -y
+function confirm_ssh_port()
+{
+  searchString="Port 22"
+  file="/etc/ssh/sshd_config"
+  if grep -Fq "$searchString" $file ; then
+    echo -e "SSH is currently set to the default port 22."
+  else
+    echo -e "Looks like you have configured a custom SSH port..."
+    echo -e
+    read -p 'Enter your custom SSH port, or hit [ENTER] for default: ' SSHPORT
+	  if [ -z "$SSHPORT" ]; then
+      SSHPORT=22
+  fi
+}
+
+echo -e "${YELLOW}Using SSH port:${GREEN}" $SSHPORT
+echo -e "${NC}"
+sleep 2
+
+echo -e "${YELLOW}=================================================================="
+echo -e "$COIN_NAME MASTERNODE INSTALLER"
+echo -e "==================================================================${NC}"
+echo -e "${YELLOW}Installing packages and updates...${NC}"
+sudo apt-get update -y &> /dev/null
+sudo apt-get install software-properties-common -y &> /dev/null
+sudo apt-get update -y &> /dev/null
+sudo apt-get upgrade -y &> /dev/null
+sudo apt-get install nano htop pwgen ufw figlet -y &> /dev/null
+echo "....."
+sudo apt-get install build-essential libtool pkg-config -y &> /dev/null
+echo "...."
+sudo apt-get install libc6-dev m4 g++-multilib -y &> /dev/null
+echo "..."
+sudo apt-get install autoconf ncurses-dev unzip git python python-zmq -y &> /dev/null
+echo ".."
+sudo apt-get install wget curl bsdmainutils automake -y &> /dev/null
+echo "."
+sudo apt-get install python-virtualenv virtualenv -y &> /dev/null
 echo "Packages complete..."
 
 WANIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
@@ -69,66 +78,131 @@ if [ "x$PASSWORD" = "x" ]; then
     PASSWORD=${WANIP}-`date +%s`
 fi
 
-#begin downloading wallet
-echo "Killing and removing all old instances of $COIN_NAME and Downloading new wallet..."
-sudo killall $COIN_DAEMON > /dev/null 2>&1
-cd /usr/bin && sudo rm $COIN_CLI $COIN_DAEMON > /dev/null 2>&1 && sleep 2 && cd
+function prepare_system() 
+{
+  clear
+  echo -e "Checking if swap space is required."
+  local PHYMEM=$(free -g | awk '/^Mem:/{print $2}')
+  
+  if [ "${PHYMEM}" -lt "2" ]; then
+    local SWAP=$(swapon -s get 1 | awk '{print $1}')
+    if [ -z "${SWAP}" ]; then
+      echo -e "${GREEN}Server is running without a swap file and has less than 2G of RAM, creating a 2G swap file.${NC}"
+      dd if=/dev/zero of=/swapfile bs=1024 count=2M
+      chmod 600 /swapfile
+      mkswap /swapfile
+      swapon -a /swapfile
+    else
+      echo -e "${GREEN}Swap file already exists.${NC}"
+    fi
+  else
+    echo -e "${GREEN}Server running with at least 2G of RAM, no swap file needed.${NC}"
+  fi
+}
 
-rm -rf $EXTRACT_DIR
-rm -rf $WALLET_TAR_FILE
-wget -U Mozilla/5.0 $WALLET_DOWNLOAD
-
-$ZIPTAR $WALLET_TAR_FILE
-cd $EXTRACT_DIR
-sudo chmod +x $COIN_CLI $COIN_DAEMON
-cp $COIN_CLI $COIN_DAEMON $COIN_PATH
-sudo chmod +× /usr/bin/anon*
+#Downloading bins
+wget -c $WALLET_DOWNLOAD -O - | tar -xz -C /usr/local/bin/
+cd /user/local/bin/anon/src/
+mv anond anon-cli /usr/local/bin/
 cd
-rm -rf $EXTRACT_DIR
-rm -rf $WALLET_TAR_FILE
-#end downloading/cleaning up wallet
 
-wget -U Mozilla/5.0 $BOOTSTRAP
-sudo mkdir $CONFIG_FOLDER
-unzip $BOOTSTRAP_ZIP -d $CONFIG_FOLDER
-rm -rf $BOOTSTRAP_ZIP
-echo "downloading chain params"
-wget $FETCHPARAMS
-sudo bash fetch-params.sh
-echo "Done fetching chain params"
-
-echo "Creating Conf File wallet"
-sudo touch $CONFIG_FOLDER/$CONFIG_FILE
-cat <<EOF > $CONFIG_FOLDER/$CONFIG_FILE
-rpcuser=$COIN_NAME
+function create_initial_config()
+{
+  echo -e "${YELLOW}CREATING INITIAL CONF FILE${NC}"
+  RPCUSER=`pwgen -1 8 -n`
+  PASSWORD=`pwgen -1 20 -n`
+  touch $CONFIG_FOLDER/$CONFIG_FILE
+  cat <<EOF > $CONFIG_FOLDER/$CONFIG_FILE
+rpcuser=$RPCUSER
 rpcpassword=$PASSWORD
 rpcallowip=127.0.0.1
-server=1
-daemon=1
-listen=1
-rpcport=$RPCPORT
 port=$PORT
-externalip=$WANIP
-addnode=$ADDNODE1
-addnode=$ADDNODE2
-addnode=$ADDNODE3
-txindex=1
+rpcport=$RPCPORT
+daemon=1
+addnode=explorer.anonfork.io
+addnode=explorer.anon.zeltrez.io
 maxconnections=256
 EOF
+}
 
-echo "Creating system service file...."
- cat << EOF > /etc/systemd/system/$COIN_NAME.service
+function download_bootstrap()
+{
+  echo -e "${YELLOW}DOWNLOADING BOOTSTRAP FOR QUICK SYNCING...${NC}"
+  wget -U Mozilla/5.0 $BOOTSTRAP
+  mkdir $CONFIG_FOLDER
+  unzip $BOOTSTRAP_ZIP -d $CONFIG_FOLDER
+  rm -rf $BOOTSTRAP_ZIP
+  clear
+}
+
+function fetch_params()
+{
+  echo -e "${YELLOW}DOWNLOADING CHAIN PARAMS${NC}"
+  wget $FETCHPARAMS
+  bash fetch-params.sh
+  clear
+}
+
+function install_sentinel()
+{
+  echo "${YELLOW}INSTALLING SENTINEL${NC}"
+  cd
+  git clone https://github.com/anonymousbitcoin/sentinel.git
+  cd sentinel
+  virtualenv ./venv
+  venv/bin/pip install -r requirements.txt
+  clear
+}
+
+#sentinel conf
+SENTINEL_CONF=$(cat <<EOF
+anon_conf=/home/$USERNAME/.anon/anon.conf
+db_name=/home/$USERNAME/sentinel/database/sentinel.db
+db_driver=sqlite
+network=mainnet
+EOF
+)
+
+function conf_sentinel()
+{
+  echo "${YELLOW}CONFIGURING SENTINEL AND CRON JOB...${NC}"
+  echo "$SENTINEL_CONF" > ~/sentinel/sentinel.conf
+  cd
+  crontab -l -echo "* * * * * cd /$USERNAME/sentinel && ./venv/bin/python bin/sentinel.py 2>&1 >> sentinel-cron.log" | crontab 
+  clear
+}  
+
+function configure_firewall()
+{ 
+  echo -e "${YELLOW}CONFIGURING FIREWALL AND ENABLING FAIL2BAN...${NC}"
+  ufw allow $PORT/tcp
+  ufw allow $RPCPORT/tcp
+  ufw allow $SSHPORT/tcp
+  ufw logging on
+  ufw default deny incoming
+  ufw default allow outgoing
+  echo "y" | ufw enable >/dev/null 2>&1
+  systemctl enable fail2ban >/dev/null 2>&1
+  systemctl start fail2ban >/dev/null 2>&1
+}
+
+function create_daemon_service()
+{
+  echo -e "${YELLOW}CREATING DAEMON SERVICE FILE...${NC}"
+  touch /etc/systemd/system/$COIN_NAME.service
+  cat << EOF > /etc/systemd/system/$COIN_NAME.service
 [Unit]
 Description=$COIN_NAME service
 After=network.target
 [Service]
-User=root
-Group=root
 Type=forking
-#PIDFile=$CONFIG_FOLDER/$COIN_NAME.pid
-ExecStart=$COIN_PATH/$COIN_DAEMON -daemon -conf=$CONFIG_FOLDER/$CONFIG_FILE -datadir=$CONFIG_FOLDER
-ExecStop=-$COIN_PATH/$COIN_CLI -conf=$CONFIG_FOLDER/$CONFIG_FILE -datadir=$CONFIG_FOLDER stop
+User=$USERNAME
+Group=$USERNAME
+WorkingDirectory=/home/$USERNAME/.anon/
+ExecStart=$COIN_PATH/$COIN_DAEMON -datadir=/home/$USERNAME/.anon/ -conf=/home/$USERNAME/.anon/$CONFIG_FILE -daemon
+ExecStop=-$COIN_PATH/$COIN_CLI stop
 Restart=always
+RestartSec=3
 PrivateTmp=true
 TimeoutStopSec=60s
 TimeoutStartSec=10s
@@ -138,91 +212,47 @@ StartLimitBurst=5
 WantedBy=multi-user.target
 EOF
 
-systemctl daemon-reload
-sleep 3
-systemctl start $COIN_NAME.service
-systemctl enable $COIN_NAME.service >/dev/null 2>&1
+  systemctl daemon-reload
+  sleep 3
+  systemctl enable $COIN_NAME.service &> /dev/null
 
-echo "Systemctl Complete...."
+  echo -e "${GREEN}STARTING DAEMON SERVICE FOR ANON${NC}"
+  systemctl start $COIN_NAME.service >/dev/null 2>&1
+  clear
+}
 
-echo "If you see *error* message, do not worry we are killing wallet again to make sure its dead"
-echo ""
-echo "=================================================================="
-echo "DO NOT CLOSE THIS WINDOW OR TRY TO FINISH THIS PROCESS "
-echo "PLEASE WAIT ~2 MINUTES UNTIL YOU SEE THE RELOADING WALLET MESSAGE"
-echo "=================================================================="
-echo ""
-
-echo "Stopping daemon again and creating final config..."
-echo "Configuring firewall..."
-#add a firewall
-sudo ufw allow $PORT/tcp
-sudo ufw allow $RPCPORT/tcp
-echo "Basic security completed..."
-
-
-echo "Installing sentinel..."
-cd $CONFIG_FOLDER
-sudo apt-get install -y git python-virtualenv
-
-git clone https://github.com/anonymousbitcoin/sentinel.git && cd sentinel
-virtualenv ./venv
-./venv/bin/pip install -r requirements.txt
-
-echo "Adding crontab jobs..."
-crontab -l > tempcron
-#echo new cron into cron file
-echo "* * * * * cd $CONFIG_FOLDER/sentinel && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1" >> tempcron
-echo "@reboot /bin/sleep 20 ; $COIN_DAEMON -daemon &" >> tempcron
-
-#install new cron file
-crontab tempcron
-rm tempcron
-echo "Sentinel Installed"
-
-echo "Restarting $COIN_NAME wallet with new configs, 30 seconds..."
-sudo chmod +x /usr/bin/anon*
-$COIN_DAEMON -daemon
-sleep 60
-
-echo "Making genkey..."
+function create_genkey()
+{
+  echo -e "${YELLOW}MAKING GENKEY...${NC}"
 GENKEY=$($COIN_CLI masternode genkey)
+clear
+}
 
-echo "Mining info..."
-$COIN_CLI getmininginfo
-$COIN_CLI stop
-
-echo "Stopping daemon again and creating final config..."
-cat <<EOF > $CONFIG_FOLDER/$CONFIG_FILE
-rpcuser=$COIN_NAME
-rpcpassword=$PASSWORD
-rpcallowip=127.0.0.1
-server=1
-daemon=1
-listen=1
-rpcport=$RPCPORT
-port=$PORT
-externalip=$WANIP
+function finalize_conf()
+{
+  cat <<EOF > $CONFIG_FOLDER/$CONFIG_FILE
 masternode=1
 masternodeprivkey=$GENKEY
-addnode=$ADDNODE1
-addnode=$ADDNODE2
-addnode=$ADDNODE3
+externalip=$WANIP
+bind=$WANIP
+logtimestamps=1
+server=1
 txindex=1
+listen=1
 maxconnections=256
 EOF
-
-sleep 30
-
-echo "Starting your ANON NODE with final details"
-
-$COIN_DAEMON -daemon
+}
 
 echo "============================================================================="
-echo "COPY THIS TO HOT WALLET CONFIG FILE AND REPLACE TxID and OUTPUT"
+echo "COPY THIS TO MASTERNODE CONF FILE AND REPLACE TxID and OUTPUT"
 echo "WITH THE DETAILS FROM YOUR COLLATERAL TRANSACTION"
-echo "MN1 $WANIP:$PORT $GENKEY TxID OUTPUT"
-echo "Courtesy of AltTank"
-echo "Updated to new binaries by dk808"
+echo -e "${YELLOW}MN1 $WANIP:$PORT $GENKEY TxID OUTPUT${NC}"
+echo "Courtesy of AltTank Fam and DK808"
+echo
+echo "FOLLOWING COMMANDS TO MANAGE $COIN_NAME SERVICE"
+echo -e "TO START- ${GREEN}systemctl start $COIN_NAME.service${NC}"
+echo -e "TO STOP - ${GREEN}systemctl stop $COIN_NAME.service${NC}"
+echo -e "STATUS  - ${GREEN}systemctl stauts $COIN_NAME.service{NC}"
+echo "IN THE EVENT SERVER REBOOTS DAEMON SERVICE WILL AUTO START"
 echo "============================================================================="
 sleep 1
