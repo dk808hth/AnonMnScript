d3dbae0e66cf8d1ba1ac015f7f979ccd00f705c3
dk808hth <44477250+dk808hth@users.noreply.github.com>
2019-04-14 13:23:44 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -15,7 +15,7 @@ COIN_PATH='/usr/local/bin'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
-NC='\033[0m
+NC='\033[0m'
 
 #stop service to update system
 echo -e "${YELLOW}STOPPING $COIN_NAME SERVICE AND UPDATING SYSTEM...${NC}"
